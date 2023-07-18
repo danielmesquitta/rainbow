f2f3c5247fc4f3f77df7cb5aa103bafb832346b3
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-18 17:25:52 -0300

    feat(server): init fastify

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,68 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Rainbow')
+  .setVersion('1.0')
+  .build();
+
+const bootstrap = async () => {
+  /**
+   * Connect with fastify.
+   */
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  /**
+   * Enable cors.
+   */
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ? JSON.parse(process.env.CORS_ORIGIN) : '*',
+    credentials: true,
+  });
+
+  /**
+   * Run validations.
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  /**
+   * Apply transform to all responses.
+   */
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  /**
+   * Run swagger.
+   */
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('docs', app, document, {
+    customCss: '.swagger-ui .renderedMarkdown code { color: #3B4151; }',
+  });
+
+  /**
+   * Run app.
+   */
+  await app.listen(process.env.PORT || 3333, process.env.HOST || '0.0.0.0');
+};
+
 bootstrap();
