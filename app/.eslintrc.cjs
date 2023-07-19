71ac4757e5b9f3da6ec18fa71adf226d2f5909e5
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-19 20:49:23 -0300

    feat(app): eslint config

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
@@ -18,10 +17,11 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-misused-promises': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
-}
+};
