00139021d99e707549e6e06f1527ab3c41fcd6ed
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-19 20:48:16 -0300

    feat(app): absolute path config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    },
+  },
 });
