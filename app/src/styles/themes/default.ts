4bb6a73dcd71afbbfd4236376db42fdba6ccacc9
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-19 20:50:29 -0300

    styles(app): global styles

---
+++ app/src/styles/themes/default.ts
@@ -0,0 +1,35 @@
+export const defaultTheme = {
+  fonts: {
+    default: "'Roboto', sans-serif",
+  },
+
+  breakpoints: {
+    lg: '1152px',
+    md: '992px',
+    sm: '768px',
+    xs: '576px',
+    xxs: '384px',
+  },
+
+  colors: {
+    white: '#FFF',
+
+    'gray-100': '#FAFAFA',
+    'gray-200': '#F3F2F2',
+    'gray-300': '#EDEDED',
+    'gray-400': '#E6E5E5',
+    'gray-500': '#D7D5D5',
+    'gray-600': '#8D8686',
+    'gray-700': '#574F4D',
+    'gray-800': '#403937',
+    'gray-900': '#272221',
+
+    'primary-300': '#F1E9C9',
+    'primary-500': '#DBAC2C',
+    'primary-700': '#C47F17',
+
+    'secondary-300': '#EBE5F9',
+    'secondary-500': '#8047F8',
+    'secondary-700': '#4B2995',
+  },
+};
